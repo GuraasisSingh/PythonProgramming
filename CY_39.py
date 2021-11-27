9a765be5c68d0f96eb2192afafc9bcc00769616c
Guraasis Singh <guraasiss@gmail.com>
2021-11-28 01:26:12 +0530

    corrected the cal text box to properly show the calender

--- CY_39.py
+++ CY_39.py
@@ -10,26 +10,26 @@ def Calculate():
     rg=int(re.get())
     tg=int(te.get())
     ci=(pg*((1+(rg/100))**tg))-pg
-    Label(text=f"{ci}",font="arial 10").place(x=100,y=160)
+    Label(text=f"{ci}",font="centurygothic 10").place(x=100,y=160)
 
-p=Label(root,text="PRINCIPLE: ",font="arial 10",bg="green",fg="white").place(x=60,y=10)
-r=Label(root,text="RATE: ",font="arial 10",bg="green",fg="white").place(x=60,y=30)
-t=Label(root,text="TIME: ",font="arial 10",bg="green",fg="white").place(x=60,y=50)
+p=Label(root,text="PRINCIPLE: ",font="arial 10",bg="green",fg="white").place(x=50,y=10)
+r=Label(root,text="RATE        : ",font="arial 10",bg="green",fg="white").place(x=50,y=30)
+t=Label(root,text="TIME         : ",font="arial 10",bg="green",fg="white").place(x=50,y=50)
 
 pv=StringVar()
 rv=StringVar()
 tv=StringVar()
 
-pe=Entry(root,font="arial 10",textvar=pv,width=8,bg="white",fg="black")
-re=Entry(root,font="arial 10",textvar=rv,width=8,bg="white",fg="black")
-te=Entry(root,font="arial 10",textvar=tv,width=8,bg="white",fg="black")
+pe=Entry(root,font="centurygothic 10",textvar=pv,width=8,bg="white",fg="black")
+re=Entry(root,font="centurygothic 10",textvar=rv,width=8,bg="white",fg="black")
+te=Entry(root,font="centurygothic 10",textvar=tv,width=8,bg="white",fg="black")
 
 pe.place(x=140, y=10)
 re.place(x=140, y=30)
 te.place(x=140, y=50)
 
-Button(root,text="Calculation",font="arial 10",bg="blue", fg="white",command=Calculate).place(x=100,y=90)
-Button(root,text="Exit",font="arial 10",bg="blue", fg="white",command=lambda:exit()).place(x=200,y=90)
+Button(root,text="Calculation",font="centurygothic 10",bg="firebrick1", fg="white",command=Calculate).place(x=100,y=90)
+Button(root,text="Exit",font="centurygothic 10",bg="firebrick1", fg="white",command=lambda:exit()).place(x=200,y=90)
 
 
 root.mainloop()
@@ -39,7 +39,7 @@ import calendar
 root=Tk()
 root.geometry("500x550")
 root.title("CALENDAR")
-root.config(bg="limegreen")
+root.config(bg="yellow2")
 
 def show():
     m=int(month.get())
@@ -65,7 +65,7 @@ y_label.place(x=120,y=50)
 year=Spinbox(root,from_=1800,to=3000,width="5")
 year.place(x=170,y=50)
 
-cal=Text(root,width=21,height=6,borderwidth=2)
+cal=Text(root,width=21,height=7,borderwidth=2)
 cal.place(x=50, y=110)
 
 show=Button(root,text="Show",font=("verdana",8,"bold"),command=show)
